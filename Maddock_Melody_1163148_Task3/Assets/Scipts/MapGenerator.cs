3fb1a8a23b9ae44a5c194458df4276077c54e545
MelodyMad <M_M337@student.usc.edu.au>
2025-09-03 19:05:11 +1000

    Octaves, Persistence, and Lacunarity
    Following the third episode, octaves were added to the PerlinNoise C# script to increase terrain realism by adding more detail to the environment. Persistence was added to create the smoothness of the terrain. A lower persistence value creates a more smooth environment. Lacunarity was added to determine how the frequency  of each octave changes in relation to the previous one. A higher lacunarity value increases the gaps between the octaves, which introduces more variation into the world. Seeds were also added so that you can generate the same map again.

--- Maddock_Melody_1163148_Task3/Assets/Scipts/MapGenerator.cs
+++ Maddock_Melody_1163148_Task3/Assets/Scipts/MapGenerator.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.UIElements;
 
 public class MapGenerator : MonoBehaviour
 {
@@ -7,14 +8,42 @@ public class MapGenerator : MonoBehaviour
     [SerializeField] private int mapHeight;
     [SerializeField] private float noiseScale;
 
+    [SerializeField] private int octaves;
+    [Range(0,1)] [SerializeField] private float persistance;
+    [SerializeField] private float lacunarity;
+
+    [SerializeField] private int seed;
+    [SerializeField] private Vector2 offset;
+
     public bool autoUpdate;
 
     public void GenerateMap()
     {
-        float[,] noiseMap = PerlinNoise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+        float[,] noiseMap = PerlinNoise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
 
-        MapDisplay display = FindObjectOfType<MapDisplay> ();
+        MapDisplay display = FindObjectOfType<MapDisplay>();
         display.DrawNoiseMap(noiseMap);
+    }
+
+    void OnValidate()
+    {
+        if (mapWidth < 1)
+        {
+            mapWidth = 1;
+        }
+        if (mapHeight < 1)
+        {
+            mapHeight = 1;
+        }
+        if (lacunarity < 1)
+        {
+            lacunarity = 1;
+        }
+        if (octaves < 0)
+        {
+            octaves = 0;
+        }
+
     }
 
 }
--- Maddock_Melody_1163148_Task3/Assets/Scipts/PerlinNoise.cs
+++ Maddock_Melody_1163148_Task3/Assets/Scipts/PerlinNoise.cs
@@ -2,16 +2,30 @@ using UnityEngine;
 
 public static class PerlinNoise
 {
-    public static float[,] GenerateNoiseMap(int mapWidth, int MapHeight, float scale, int octaves, float persistance, float lacunarity)
+    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, int seed, float scale, int octaves, float persistance, float lacunarity, Vector2 offset)
     {
-        float[,] noiseMap = new float [mapWidth, MapHeight];
+        float[,] noiseMap = new float [mapWidth, mapHeight];
+
+        System.Random prng = new System.Random(seed);
+        Vector2[] octaveOffsets = new Vector2[octaves];
+        for (int i = 0; i < octaves; i++)
+        {
+            float offsetX = prng.Next(-100000, 100000) + offset.x;
+            float offsetY = prng.Next(-100000, 100000) + offset.y;
+            octaveOffsets[i] = new Vector2(offsetX, offsetY);
+        }
 
         if (scale <=0)
         {
             scale = 0.0001f;
         }
 
-        for (int y = 0; y < MapHeight; y++)
+        float maxNoiseHeight = float.MinValue;
+        float minNoiseHeight = float.MaxValue;
+        float halfWidth = mapWidth / 2f;
+        float halfHeight = mapHeight / 2f;
+
+        for (int y = 0; y < mapHeight; y++)
         {
             for (int x = 0; x < mapWidth; x++)
             {
@@ -21,18 +35,36 @@ public static class PerlinNoise
 
                 for (int i = 0; i < octaves; i++)
                 {
-                    float xCoord = x / scale * frequency;
-                    float yCoord = y / scale * frequency;
+                    float xCoord = (x-halfWidth) / scale * frequency + octaveOffsets[i].x;
+                    float yCoord = (y-halfHeight) / scale * frequency + octaveOffsets[i].y;
 
-                    float perlinValue = Mathf.PerlinNoise(xCoord, yCoord);
+                    float perlinValue = Mathf.PerlinNoise(xCoord, yCoord) * 2 - 1;
                     noiseMap[x, y] = perlinValue;
                     noiseHeight += perlinValue * amplitude;
 
                     amplitude *= persistance;
                     frequency *= lacunarity;
                 }
+                if (noiseHeight > maxNoiseHeight)
+                {
+                    maxNoiseHeight = noiseHeight;
+                }
+                else if (noiseHeight < minNoiseHeight)
+                {
+                    minNoiseHeight = noiseHeight;
+                }
+                noiseMap[x, y] = noiseHeight;
             }
         }
+
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                noiseMap[x, y] = Mathf.InverseLerp(minNoiseHeight, maxNoiseHeight, noiseMap[x, y]);
+            }
+        }
+
         return noiseMap;
     }
 
