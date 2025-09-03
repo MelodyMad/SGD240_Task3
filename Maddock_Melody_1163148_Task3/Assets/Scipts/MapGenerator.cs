23944714f94603d31596902bd2349fbfba0a48ea
MelodyMad <M_M337@student.usc.edu.au>
2025-09-03 21:27:42 +1000

    Implementing Colour
    A TextureGenerator C# script was created that allows the editor to switch between a classic Perlin noise map and a coloured version of the map. With this script the editor can create their own custom colours to appear between set defined heights within the editor so that you have have varying colours within your world.

--- Maddock_Melody_1163148_Task3/Assets/Scipts/MapGenerator.cs
+++ Maddock_Melody_1163148_Task3/Assets/Scipts/MapGenerator.cs
@@ -4,6 +4,9 @@ using UnityEngine.UIElements;
 
 public class MapGenerator : MonoBehaviour
 {
+    public enum DrawMode { NoiseMap, ColourMap };
+    public DrawMode drawmode;
+
     [SerializeField] private int mapWidth;
     [SerializeField] private int mapHeight;
     [SerializeField] private float noiseScale;
@@ -17,12 +20,38 @@ public class MapGenerator : MonoBehaviour
 
     public bool autoUpdate;
 
+    public TerrainType[] reigons;
+
     public void GenerateMap()
     {
         float[,] noiseMap = PerlinNoise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
 
+        Color[] colourMap = new Color[mapWidth * mapHeight];
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                float currentHeight = noiseMap[x, y];
+                for (int i = 0; i < reigons.Length; i++)
+                {
+                    if (currentHeight <= reigons[i].height)
+                    {
+                        colourMap[y * mapWidth + x] = reigons[i].colour;
+                        break;
+                    }
+                }
+            }
+        }
+
         MapDisplay display = FindObjectOfType<MapDisplay>();
-        display.DrawNoiseMap(noiseMap);
+        if (drawmode ==DrawMode.NoiseMap)
+        {
+            display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
+        }
+        else if (drawmode == DrawMode.ColourMap)
+        {
+            display.DrawTexture(TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
+        }
     }
 
     void OnValidate()
@@ -46,4 +75,11 @@ public class MapGenerator : MonoBehaviour
 
     }
 
+    [System.Serializable]
+    public struct TerrainType
+    {
+        public string name;
+        public float height;
+        public Color colour;
+    }
 }
